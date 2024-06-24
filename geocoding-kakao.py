9dd2619c9ae0753497c07be6490e7e8c56d496b0
Yubin Lee <42401542+lucyubin@users.noreply.github.com>
2024-06-24 12:57:54 +0900

    Update geocoding-kakao.py

--- geocoding-kakao.py
+++ geocoding-kakao.py
@@ -6,6 +6,7 @@ import geokakao as gk
 data = pd.read_csv('*.csv', encoding='euc-kr')
 data = data.head(10)
 
-gk.add_coordinates_to_dataframe(data, '상세주소')
+gk.add_coordinates_to_dataframe(data, '주소')
 print(data)
+
 data.to_csv('*.csv', index=False, encoding='euc-kr')
